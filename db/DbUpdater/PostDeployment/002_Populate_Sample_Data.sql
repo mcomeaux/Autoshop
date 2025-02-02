fbde691f7644527f8f1490837bc601140330b9e1
Marc <mcomeaux@gmail.com>
2025-02-02 14:56:47 -0600

    Sample data for Vehicles

--- db/DbUpdater/PostDeployment/002_Populate_Sample_Data.sql
+++ db/DbUpdater/PostDeployment/002_Populate_Sample_Data.sql
@@ -4,9 +4,25 @@ INSERT INTO [Customers] (
     [Email],
     [PhoneNumber],
     [Address]
-) values (
+) VALUES (
 'Test Testerson',
 'testerson@gmail.com',
 '1234567890',
 '302 Brown St. Atlanta GA'
-)
+);
+
+INSERT INTO [Vehicles] (
+    [Make],
+    [Model],
+    [Year],
+    [Color],
+    [Description],
+    [CustomerId]
+) VALUES (
+    'Honda',
+    'Civic',
+    1991,
+    'Blue',
+    'Car is setup for Autocross',
+    1
+);
